8a4af1353be688d3318f6b64712e9f4c3735f27a
Devon DeJohn <ddejohn@pm.me>
2021-07-15 23:43:36 -0600

    implements alternate class structure

--- dndShell.py
+++ dndShell.py
@@ -4,230 +4,132 @@ from random import randint
 import yaml
 
 
-class BaseAbility:
-    def __init__(self, base_score: int, saving: int) -> None:
-        self.base_score = base_score
-        self.saving = saving
-    
+class Skill:
+    def __init__(self, base: int, proficient: bool) -> None:
+        self.base = base
+        self.proficient = proficient
+
+    @property
+    def modifier(self):
+        return self.base // 2 - 5
+
     def __str__(self):
         return "\n".join(f"    {k}: {v}" for k, v in self.__dict__.items())
 
 
-class Strength(BaseAbility):
-    def __init__(self, *, base_score: int,
-                          saving: int,
-                          athletics: int) -> None:
-        super().__init__(base_score, saving)
-        self.athletics = athletics
-
-
-class Dexterity(BaseAbility):
-    def __init__(self, *, base_score: int,
-                          saving: int,
-                          acrobatics: int,
-                          sleight_of_hand: int,
-                          stealth: int) -> None:
-        super().__init__(base_score, saving)
-        self.acrobatics = acrobatics
-        self.sleight_of_hand = sleight_of_hand
-        self.stealth = stealth
-
-
-class Constitution(BaseAbility):
-    def __init__(self, *, base_score: int, saving: int) -> None:
-        super().__init__(base_score, saving)
-
-
-class Intelligence(BaseAbility):
-    def __init__(self, *, base_score: int,
-                          saving: int,
-                          arcana: int,
-                          history: int,
-                          investigation: int,
-                          nature: int,
-                          religion: int) -> None:
-        super().__init__(base_score, saving)
-        self.arcana = arcana
-        self.history = history
-        self.investigation = investigation
-        self.nature = nature
-        self.religion = religion
-
-
-class Wisdom(BaseAbility):
-    def __init__(self, *, base_score: int,
-                          saving: int,
-                          animal_handling: int,
-                          insight: int,
-                          medicine: int,
-                          perception: int,
-                          survival: int) -> None:
-        super().__init__(base_score, saving)
-        self.animal_handling = animal_handling
-        self.insight = insight
-        self.medicine = medicine
-        self.perception = perception
-        self.survival = survival
-
-
-class Charisma(BaseAbility):
-    def __init__(self, *, base_score: int,
-                          saving: int,
-                          deception: int,
-                          intimidation: int,
-                          performance: int,
-                          persuasion: int) -> None:
-        super().__init__(base_score, saving)
-        self.deception = deception
-        self.intimidation = intimidation
-        self.performance = performance
-        self.persuasion = persuasion
+class Ability(Skill):
+    def __init__(self, base: int, saving: int, proficient: bool) -> None:
+        super().__init__(base, proficient)
+        self.saving = saving
 
 
 class dndShell(cmd.Cmd):      
-    # Grammar: (<skill> | <ability>) ("-", ("c" | "s"), [[" -"], ("a" | "d")])
-    # `deception -ca`, skill check with advantage
-    # `charisma -s -d`, base_score ability saving throw with disadvantage
-    # `insight -c`, skill check
     intro = "Welcome to the DnD shell! Type help or ? to list commands.\n"
     prompt = "(DnD) > "
-    adv = False
-    disadv = False
-    check = True
-    saving = False
-
-    def dice(self, num_sides, num_dice=1):
-        return [randint(1, num_sides) for _ in range(num_dice)]
+    advantage = False
+    disadvantage = False
+    verbose = False
 
     def precmd(self, line) -> None:
         """Strip args"""
         args = line.split()
-        self.adv = "-a" in args
-        self.disadv = "-d" in args
-        self.saving = "-s" in args
-        self.check = "-c" in args or not self.saving
+        self.advantage = "-a" in args
+        self.disadvantage = "-d" in args
+        self.verbose = "-v" in args
+        return " ".join(args[:2])
+    
+    def do_q(self, arg):
+        """quit"""
+        return 1
 
-        if args[0] != "display":
-            print(f"rolling: {args[0]}" + " saving throw"*self.saving
-                                        + " check"*self.check
-                                        + " with advantage"*self.adv
-                                        + " with disadvantage"*self.disadv)
-        return args[0]
-
-    def roll(self, ability):
-        rolls = self.dice(20, 1 + (self.adv ^ self.disadv))
-        result = min(rolls) if ((not self.adv) and self.disadv) else max(rolls)
-        modifier = (ability // 2) - 5
-        print(f"base ability score: {ability}")
-        print(f"modifier: " + "+"*(modifier >= 0) + f"{modifier}")
-        print(f"dice rolls: {rolls}")
-        print(f"roll used: {result}")
-        if result == 20:
-            print("\nnat 20!!!\n")
-        if result == 1:
-            print("\nnat 1!!!\n")
-        print(f"result with modifier: {result + modifier}\n")
-
-    def do_display(self, args):
+    def do_stats(self, arg):
+        """show player stats"""
         print(self)
+        return 0
 
-    def do_strength(self, args):
-        if self.check:
-            return self.roll(self.strength.base_score)
-        return self.roll(self.strength.saving)
+    def do_check(self, arg):
+        """check <ability or skill> (-a | -d) (-v)"""
+        result = self.check(arg,
+                            self.verbose,
+                            self.advantage,
+                            self.disadvantage)
+        self.results(arg, "check", result, self.advantage, self.disadvantage)
+        return 0
 
-    def do_dexterity(self, args):
-        if self.check:
-            return self.roll(self.dexterity.base_score)
-        return self.roll(self.dexterity.saving)
+    def do_save(self, arg):
+        """save <ability> (-a | -d) (-v)"""
+        result = self.save(arg,
+                           self.verbose,
+                           self.advantage,
+                           self.disadvantage)
+        self.results(arg, "save", result, self.advantage, self.disadvantage)
+        return 0
 
-    def do_constitution(self, args):
-        if self.check:
-            return self.roll(self.constitution.base_score)
-        return self.roll(self.constitution.saving)
+    def results(self, arg, roll_type, result, a, d):
+        print(f"{arg} {roll_type}" \
+              + (" with advantage"*a + " with disadvantage"*d)*(a ^ d) \
+              + f": {result}")
 
-    def do_intelligence(self, args):
-        if self.check:
-            return self.roll(self.intelligence.base_score)
-        return self.roll(self.intelligence.saving)
-
-    def do_wisdom(self, args):
-        if self.check:
-            return self.roll(self.wisdom.base_score)
-        return self.roll(self.wisdom.saving)
-
-    def do_charisma(self, args):
-        if self.check:
-            return self.roll(self.charisma.base_score)
-        return self.roll(self.charisma.saving)
-
-    def do_athletics(self, args):
-        return self.roll(self.strength.athletics)
-
-    def do_acrobatics(self, args):
-        return self.roll(self.dexterity.acrobatics)
-
-    def do_sleight_of_hand(self, args):
-        return self.roll(self.dexterity.sleight_of_hand)
-
-    def do_stealth(self, args):
-        return self.roll(self.dexterity.stealth)
-
-    def do_arcana(self, args):
-        return self.roll(self.intelligence.arcana)
-
-    def do_history(self, args):
-        return self.roll(self.intelligence.history)
-
-    def do_investigation(self, args):
-        return self.roll(self.intelligence.investigation)
-
-    def do_nature(self, args):
-        return self.roll(self.intelligence.nature)
-
-    def do_religion(self, args):
-        return self.roll(self.intelligence.religion)
-
-    def do_animal_handling(self, args):
-        return self.roll(self.wisdom.animal_handling)
-
-    def do_insight(self, args):
-        return self.roll(self.wisdom.insight)
-
-    def do_medicine(self, args):
-        return self.roll(self.wisdom.medicine)
-
-    def do_perception(self, args):
-        return self.roll(self.wisdom.perception)
-
-    def do_survival(self, args):
-        return self.roll(self.wisdom.survival)
-
-    def do_deception(self, args):
-        return self.roll(self.charisma.deception)
-
-    def do_intimidation(self, args):
-        return self.roll(self.charisma.intimidation)
-
-    def do_performance(self, args):
-        return self.roll(self.charisma.performance)
-
-    def do_persuasion(self, args):
-        return self.roll(self.charisma.persuasion)
 
 
 class Player(dndShell):
     def __init__(self, filepath: str) -> None:
         with open(filepath, "r") as player_file:
             player_data = yaml.safe_load(player_file.read())
+
+        self.name = player_data["name"]
+        self.race = player_data["race"]
+        self.occupation = player_data["class"]
+        self.level = player_data["level"]
+        self.hp = player_data["hp"]
+        self.bonus = player_data["bonus"]
+
+        for ability, stats in player_data.get("abilities").items():
+            setattr(self, ability, Ability(**stats))
+    
+        for skill, stats in player_data.get("skills").items():
+            setattr(self, skill, Skill(**stats))
+
         super().__init__()
-        self.strength = Strength(**player_data["strength"])
-        self.dexterity = Dexterity(**player_data["dexterity"])
-        self.constitution = Constitution(**player_data["constitution"])
-        self.intelligence = Intelligence(**player_data["intelligence"])
-        self.wisdom = Wisdom(**player_data["wisdom"])
-        self.charisma = Charisma(**player_data["charisma"])
         self.cmdloop()
+
+    def check(self, attribute, verbose, advantage, disadvantage):
+        attribute = getattr(self, attribute)
+        modifier = attribute.modifier + self.bonus*attribute.proficient
+        if verbose:
+            self.verbose_roll(attribute.proficient,
+                              self.bonus*attribute.proficient,
+                              attribute.modifier,
+                              modifier)
+        return self.roll(verbose, advantage, disadvantage) + modifier
+
+    def save(self, attribute, verbose, advantage, disadvantage):
+        attribute = getattr(self, attribute)
+        saving_bonus = attribute.saving // 2 - 5
+        modifier = saving_bonus + self.bonus*attribute.proficient
+        if verbose:
+            self.verbose_roll(attribute.proficient,
+                              self.bonus*attribute.proficient,
+                              saving_bonus,
+                              modifier)
+        return self.roll(verbose, advantage, disadvantage) + modifier
+
+    def roll(self, verbose, advantage: bool, disadvantage: bool) -> int:
+        dice = self.dice(20, 1 + (advantage ^ disadvantage))
+        d20 = min(dice) if ((not advantage) and disadvantage) else max(dice)
+        if verbose:
+            print(f"rolls: {', '.join(map(str, dice))}")
+            print(f"d20: {d20}")
+        return d20
+
+    def dice(self, num_sides, num_dice=1):
+        return [randint(1, num_sides) for _ in range(num_dice)]
+
+    def verbose_roll(self, prof, prof_bonus, base_bonus, modifier):
+        print(f"proficient: {prof}")
+        print(f"proficiency bonus: {prof_bonus}")
+        print(f"base bonus: {base_bonus}")
+        print(f"modifier: {modifier}")
 
     def __str__(self):
         return "\n".join(f"\n{k}:\n{v}" for k, v in self.__dict__.items())
